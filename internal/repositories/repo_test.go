71f737c6027d66ac1ce6cf663ba71e05df5c8e10
Kellswork <kellogbonna@gmail.com>
2024-06-20 21:38:11 +0100

    fix test files, test not running

--- internal/repositories/repo_test.go
+++ internal/repositories/repo_test.go
@@ -1,55 +1,57 @@
 package repositories
 
-// "context"
-// "fmt"
-// "os"
-// "testing"
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
 
-// _ "embed"
+	_ "embed"
 
-// "github.com/checkspeed/sc-backend/internal/model"
-// "github.com/google/uuid"
-// "github.com/joho/godotenv"
-// _ "github.com/lib/pq"
-// "github.com/stretchr/testify/assert"
-// "github.com/stretchr/testify/require"
+	"github.com/checkspeed/sc-backend/internal/models"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
-// func Test_CreateSpeedtestResults(t *testing.T) {
-// 	err := godotenv.Load()
-// 	require.NoError(t, err)
-// 	dbUrl := os.Getenv("TEST_DB_URL")
-// 	ctx := context.Background()
-// 	store, err := NewStore(dbUrl)
-// 	require.NoError(t, err)
+func Test_CreateSpeedtestResults(t *testing.T) {
+	err := godotenv.Load()
+	require.NoError(t, err)
+	dbUrl := os.Getenv("DB_URL")
+	ctx := context.Background()
+	store, err := NewStore(dbUrl)
+	require.NoError(t, err)
 
-// 	defer store.CloseConn(ctx)
+	defer store.CloseConn(ctx)
 
-// 	sampleResult := model.SpeedtestResults{
-// 		ID:            uuid.NewString(),
-// 		DownloadSpeed: 15000,
-// 		UploadSpeed:   8000,
-// 		Latency:       27,
-// 		ISP:           "test",
-// 	}
-// 	err = store.CreateSpeedtestResults(ctx, &sampleResult)
-// 	assert.NoError(t, err)
+	sampleResult := models.SpeedTestResult{
+		ID:            uuid.NewString(),
+		DownloadSpeed: 15000,
+		UploadSpeed:   8000,
+		Latency:       27,
+		ISP:           "test",
+	}
+	err = store.CreateSpeedtestResult(ctx, &sampleResult)
+	assert.NoError(t, err)
 
-// 	_, err = store.GetSpeedtestResults(ctx, GetSpeedtestResultsFilter{})
-// 	assert.NoError(t, err)
-// 	// fmt.Println(resp)
-// }
+	_, err = store.GetSpeedTestResults(ctx, GetSpeedTestResultsFilter{})
+	assert.NoError(t, err)
+	// fmt.Println(resp)
+}
 
-// func Test_GetSpeedtestResults(t *testing.T) {
-// 	err := godotenv.Load()
-// 	require.NoError(t, err)
-// 	dbUrl := os.Getenv("TEST_DB_URL")
-// 	ctx := context.Background()
-// 	store, err := NewStore(dbUrl)
-// 	require.NoError(t, err)
+func Test_GetSpeedtestResults(t *testing.T) {
+	err := godotenv.Load()
+	require.NoError(t, err)
+	dbUrl := os.Getenv("DB_URL")
+	ctx := context.Background()
+	store, err := NewStore(dbUrl)
+	require.NoError(t, err)
 
-// 	defer store.CloseConn(ctx)
+	defer store.CloseConn(ctx)
 
-// 	resp, err := store.GetSpeedtestResults(ctx, GetSpeedtestResultsFilter{})
-// 	assert.NoError(t, err)
-// 	fmt.Println(resp)
-// }
+	resp, err := store.GetSpeedTestResults(ctx, GetSpeedTestResultsFilter{})
+	assert.NoError(t, err)
+	fmt.Println(resp)
+}
